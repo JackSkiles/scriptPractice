4fcf2e7e5ae4e8bbcb54db60fc9180c22997c93e
Jack Skiles <jackskiles420@gmail.com>
2020-04-21 22:08:01 -0400

    some modifications

--- script.js
+++ script.js
@@ -61,18 +61,22 @@ document.body.style.fontFamily = "sans-serif";
 const div = document.createElement("main");
 div.style.display = "flex";
 div.style.margin = "10px 10px 0 10px";
-div.style.backgroundColor = "#ccc";
-div.style.justifyContent = "center";
+div.style.height = "100vh";
+div.style.flexWrap = "wrap";
+div.style.flexDirection = "column";
+div.style.alignItems = "flex-start";
 
 const div2 = document.createElement("div");
 div2.style.display= "flex";
 div2.style.flexWrap = "wrap";
-div2.style.width = "70%";
+div2.style.width = "95%";
+div2.style.backgroundColor = "#ccc";
 div.appendChild(div2);
 
 const heading3 = document.createElement("h1");
 heading3.style.display = "flex";
 heading3.textContent = "Curse of the current Reviews";
+heading3.style.color = "grey";
 div2.appendChild(heading3);
 
 const p1 = document.createElement("p");
@@ -81,6 +85,43 @@ p1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
 div2.appendChild(p1);
 
 
-main.appendChild(div);
+const heading4 = document.createElement("h1");
+heading4.style.display = "flex";
+heading4.style.padding = "20px 0 0 20px";
+heading4.style.color = "#65a6f3";
+heading4.textContent = "Hello WatchKit";
+div.appendChild(heading4);
 
+
+const p2 = document.createElement("p");
+p2.style.display = "flex";
+p2.style.padding = "20px 0 0 20px";
+p2.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas porta ipsum, suscipit faucibus nunc congue nec. In consequat, massa a posuere volutpat, ex elit placerat justo, vel posuere dolor ligula in magna. Ut facilisis sem quis massa tristique, et dictum metus imperdiet. Donec ut purus massa. Mauris maximus quam rutrum, posuere sapien ac, ornare quam. Aenean volutpat mauris tincidunt malesuada ullamcorper. Vivamus elementum, arcu et posuere molestie, erat ligula lobortis diam, in convallis tellus elit pellentesque tortor. Nam feugiat elementum sem vitae gravida. Nullam a est id est faucibus venenatis. Donec luctus ac tortor nec maximus."
+div.appendChild(p2);
+
+const div3 = document.createElement("div");
+div3.style.display = "flex";
+div3.style.alignItems = "center";
+div3.style.padding = "20px 0 0 20px";
+div3.style.color = "white";
+div3.style.width = "100%";
+div3.style.fontSize = "10px";
+div3.style.backgroundColor = "orange";
+div.appendChild(div3);
+
+const div4 = document.createElement("div");
+div4.style.display = "flex";
+div4.style.width = "20%";
+div4.style.justifyContent = "space-between";
+div3.appendChild(div4);
+
+const header4 = document.createElement("h1");
+header4.textContent = "12 comments";
+div4.appendChild(header4);
+
+const header5 = document.createElement("h1");
+header5.textContent = "124 likes";
+div4.appendChild(header5);
+
+main.appendChild(div);
 
